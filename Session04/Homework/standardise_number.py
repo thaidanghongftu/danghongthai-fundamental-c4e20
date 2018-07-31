1c58838bb2897aa3287e4a76854b433b4eb60ab8
thaidanghongftu <thaidanghongftu@gmail.com>
2018-07-31 19:02:38 +0700

    Delete standardise_number.py

--- Session04/Homework/standardise_number.py
+++
@@ -1,9 +0,0 @@
-balance_number = input("Enter your balance: ") # Nhập dạng text vào 
-split_string = list(balance_number)
-print(split_string)
-
-split_string.insert(-3,",")
-print(split_string)
-
-
-print("Độ dài từ khoá: " , len(balance_number))
